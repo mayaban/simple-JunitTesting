2c21abb54cf62bee64f7794702ec87ce57490a92
mayaban <shubhaban@yahoo.com>
2023-03-06 22:24:07 -0500

    Added different cases of Parameterised Test and Repetitive Test

--- src/test/java/org/example/ContactManagerTest.java
+++ src/test/java/org/example/ContactManagerTest.java
@@ -1,8 +1,18 @@
 package org.example;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.Clock;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -70,5 +80,73 @@ class ContactManagerTest {
         });
     }
 
+    @Test
+    @DisplayName("Should create only on MacOs ")
+    @EnabledOnOs(value= OS.MAC,disabledReason = "Only on Windows os")
+    public void shouldThrowRuntimeExceptionWhenPhoneNumberIsNullMacOs(){
+        Assertions.assertThrows(RuntimeException.class,()->{
+            contactManager.addContact("Ram","Thapa",null);
+        });
+    }
+
+    @RepeatedTest(value=5,name="Testing on repeat {currentRepetition} of {totalRepetitions}" )
+    @DisplayName("Should test for repetitive test")
+    public void shouldThrowRuntimeExceptionWhenPhoneNumberIsNullRepeatedTest(){
+        Assertions.assertThrows(RuntimeException.class,()->{
+            contactManager.addContact("Ram","Thapa",null);
+        });
+    }
+
+    @ParameterizedTest
+    @DisplayName("Examples of Parameterised testing ")
+    @ValueSource(strings ={"9849801073","9876510234","+9889013462"})
+    public void shouldCreateContactParameterisedTest(String phoneNumber){
+        // ContactManager contactManager=new ContactManager();
+        contactManager.addContact("Ram","Thapa",phoneNumber);
+        Assertions.assertFalse(contactManager.getAllContact().isEmpty());
+        Assertions.assertEquals(1,contactManager.getAllContact().size());
+       /* Assertions.assertTrue(contactManager.getAllContact().stream()
+                .filter(contact -> contact.getFirstName().equals("Ram") &&
+                        contact.getLastName().equals("Thapa") &&
+                        contact.getPhoneNumber().equals("9849801073"))
+                .findAny()
+                .isPresent());*/
+
+
+    }
+
+    @ParameterizedTest
+    @DisplayName("Examples of Parameterised testing ")
+    @MethodSource("phoneNumberList")
+    public void shouldCreateContactParameterisedTestMethodSource(String phoneNumber) {
+        // ContactManager contactManager=new ContactManager();
+        contactManager.addContact("Ram", "Thapa", phoneNumber);
+        Assertions.assertFalse(contactManager.getAllContact().isEmpty());
+        Assertions.assertEquals(1, contactManager.getAllContact().size());
+    }
+
+    @ParameterizedTest
+    @DisplayName("Examples of Parameterised testing ")
+    @CsvSource({"9849801073","9876510234","+9889013462"})
+    public void shouldCreateContactParameterisedTestCsvSource(String phoneNumber) {
+        // ContactManager contactManager=new ContactManager();
+        contactManager.addContact("Ram", "Thapa", phoneNumber);
+        Assertions.assertFalse(contactManager.getAllContact().isEmpty());
+        Assertions.assertEquals(1, contactManager.getAllContact().size());
+    }
+
+    @ParameterizedTest
+    @DisplayName("Examples of Parameterised testing ")
+    @CsvFileSource(resources = "/data.csv")
+    public void shouldCreateContactParameterisedTestCsvFileSource(String phoneNumber) {
+        // ContactManager contactManager=new ContactManager();
+        contactManager.addContact("Ram", "Thapa", phoneNumber);
+        Assertions.assertFalse(contactManager.getAllContact().isEmpty());
+        Assertions.assertEquals(1, contactManager.getAllContact().size());
+    }
+
+    public static List<String> phoneNumberList(){
+        return Arrays.asList("9849801073","9876510234","+9889013462");
+    }
 
 }
